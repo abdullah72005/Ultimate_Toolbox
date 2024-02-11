c2547993cb67b1ab80ea1d3098127bc11dffc502
root <root@heshamezzat>
2024-02-11 14:27:06 +0200

    added layout.html
    added more files

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
