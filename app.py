d832f8b1a6e4c0d4644a6468ab8e6692ba965e61
root <root@heshamezzat>
2024-02-12 17:46:18 +0200

    edited app.py and added more helper functions
    made separate list for image and txt, made outputChoices variable, added aplogy and remove_folder_type in helpers, edited bugs and implementation details in both files

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
-from helpers.convert import deleteFiles
+from helpers.convert import deleteFiles, apology, remove_folder_type
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.config['UPLOAD_DIRECTORY'] = 'uploads/'
+
+# moved uploads to static file
+app.config['UPLOAD_DIRECTORY'] = 'static/uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16MB
-app.config['ALLOWED_EXTENSIONS'] = {'.jpg', '.jpeg', '.png', '.SVG', '.TIFF', '.TIFDoc', '.Docx', '.pdf', '.PPT', '.PPTX'}
+
+# edited a typo
+app.config['ALLOWED_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.SVG', '.TIFF', '.TIF', '.Doc', '.Docx', '.pdf', '.PPT', '.PPTX']
+
+# add allowed image and text separate variables
+app.config['IMAGE_EXTENTIONS'] = ['.jpg', '.jpeg', '.png', '.SVG', '.TIFF', '.TIF']
+app.config['TEXT_EXTENTIONS'] = ['.Doc', '.Docx', '.pdf', '.PPT', '.PPTX']
 
 @app.route("/")
 def index():
@@ -30,26 +38,49 @@ def conversion():
             # Retrieve the file from the request
             file = request.files['file']
             
-            # Extract the file extension
-            extension = os.path.splitext(file.filename)[1]
+            # Extract the file extension temporarly until we user python-magic
+            extension: str = os.path.splitext(file.filename)[1]
 
             # Check if a file is provided
             if file:
+
                 # Check if the file extension is in the allowed extensions set
                 if extension not in app.config['ALLOWED_EXTENSIONS']:
-                    return 'File is not an allowed image type.'
+
+                    # load apology for invalid extenion
+                    return apology("invalid file type")
                 
                 # Save the file to the specified directory
                 file.save(os.path.join(
                     app.config['UPLOAD_DIRECTORY'],
                     secure_filename(file.filename)
                 ))     
-                # Convert file
+
+            # make variable with desired output choices
+            outputChoices = []
+
+            # if file is image let output choices be image options and remove the file extention
+            if extension in app.config['IMAGE_EXTENTIONS']:
+                outputChoices = remove_folder_type(app.config['IMAGE_EXTENTIONS'], extension)
+
+            # if file is txt let output choices be txt options and remove the file extention
+            if extension in app.config['TEXT_EXTENTIONS']:
+                outputChoices = remove_folder_type(app.config['TEXT_EXTENTIONS'], extension)
+
+
+
+                # Convert file TODO
+
+
                 # delete file in folder 
-                deleteFiles(app)
+            deleteFiles(app)
+
+
         # Handle the case where the file size exceeds the limit
         except RequestEntityTooLarge:
-            return 'File is larger than the 16mb limit.'
+
+            # load apology for invalid file size
+            return apology('File is larger than the 16mb limit.')
         
         # Return a success message after the file has been uploaded
         return 'File has been uploaded successfully.'
--- helpers/convert.py
+++ helpers/convert.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, flash, redirect, render_template, request, session
 
 def deleteFiles(app):
-    uploadFolder = app.config['UPLOAD_DIRECTORY']
+    # app.config['UPLOAD_DIRECTORY'] was not innitialized
+    uploadFolder = 'static/uploads/'
     try:
         # List all files in the uploads folder
         files = os.listdir(uploadFolder)
@@ -15,3 +16,18 @@ def deleteFiles(app):
     except Exception as e:
         # Handle exceptions, e.g., if the folder doesn't exist
         return 'No files to delete'
+
+# apology function for wrong input
+def apology(message, code=400):
+
+    # render apology
+    return render_template("apology.html", top=code, bottom=message), code
+
+
+
+# function that ensures user doesnt change to the same file type as if (jpg to jpg)
+def remove_folder_type(test_list, item): 
+  
+    # using list comprehension loop on everything minus the file type 
+    res = [i for i in test_list if i != item] 
+    return res 
