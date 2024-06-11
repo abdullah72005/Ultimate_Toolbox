a4a5ca0cc53803e024bceefbfd1bc99b0f0e24f3
abdullah hesham <abdullahhesham005@gmail.com>
2024-06-12 00:51:07 +0300

    made the page skeleton qr 1.0

--- helpers/qr.py
+++ helpers/qr.py
@@ -8,7 +8,7 @@ def convqr(link, output_path):
 
     # create the qrcode
     url = pyqrcode.create(link)
-    url.png(output_path, scale = 8)
+    url.png(output_path, scale = 20)
     return output_path
 
 # check if link is in valid format
